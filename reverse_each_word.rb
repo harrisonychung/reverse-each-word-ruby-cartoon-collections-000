2e7ea2987e9ed57f02647201af3db5caa4092460
Harrison Chung <harrison.y.chung@gmail.com>
2020-10-11 19:45:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
 def reverse_each_word ("Hello there, and how are you?")
-  reverse_each_Word. each do |sentence|
+  reverse_each_word.each do |sentence|
     sentence.reverse
   end
+
+reverse_each_word.collect { |sentence.reverse|}
+
+
+ 
